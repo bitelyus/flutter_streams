c96d84b88949307c9efb8bdaa5bb5b944b9c93ee
Miguel <miguel@unita.es>
2021-03-21 15:57:04 +0100

    Comentarios mejorados :)

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # streams
 
-A new Flutter project.
+Ejemplo de uso básico de Stream en flutter.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This project is just an very basic example of the usage of Streams in flutter.
 
-A few resources to get you started if this is your first Flutter project:
+We will use 4 different Streams in the same Screen ... using an BLoC componet!! :)
 
+1. Stream of ints
+2. Stream of odd ints
+3. Stream of even ints
+4. Stream of String ...
+
+### resources
+
+- [Video: Construyendo aplicacioens móviles reactivas - Google i/o'18 @ 18"](https://www.youtube.com/watch?v=RS36gBEp8OI)
 - [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
 
--- lib/bloc/ContadoresBLoC.dart
+++ lib/bloc/ContadoresBLoC.dart
@@ -2,38 +2,56 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 class ContadoresBLoC {
+  /// DEFINIMOS NUESTRA ZONA DE ATRIBUTOS
 
+  /// Nuestro contador para enteros
   int entero = 0;
+
+  /// Nuestro contador para número impares
   int enteroImpar = 1;
+
+  /// Nuestro contador para guardar numeros enteros pares
   int enteroPar = 2;
 
+
+  /// ZONA DE STREAMS
+
   // CONTROLADOR > TRAMPA > EMBUDO > STREAM
+
   final StreamController<int> _intController = StreamController<int>();
   final BehaviorSubject<int> _enterosSubject = BehaviorSubject<int>();
+
   Sink<int> get embudoEnteros => _intController.sink;
+
   Stream<int> get enterosStream => _enterosSubject.stream;
 
   // CONTROLADOR > TRAMPA > EMBUDO > STREAM
   final StreamController<int> _intOddController = StreamController<int>();
   final BehaviorSubject<int> _enterosOddSubject = BehaviorSubject<int>();
+
   Sink<int> get embudoEnterosImpar => _intOddController.sink;
+
   Stream<int> get enterosStreamImpar => _enterosOddSubject.stream;
 
   // CONTROLADOR > TRAMPA > EMBUDO > STREAM
   final StreamController<int> _intEvenController = StreamController<int>();
   final BehaviorSubject<int> _enterosEvenSubject = BehaviorSubject<int>();
+
   Sink<int> get embudoEnterosPar => _intEvenController.sink;
+
   Stream<int> get enterosStreamPar => _enterosEvenSubject.stream;
 
-
+  // CONTROLADOR PARA STRINGS > TRAMPA > EMBUDO > STREAM
   final StreamController<String> _stringController = StreamController();
   final BehaviorSubject<String> _stringSubjet = BehaviorSubject<String>();
+
   Sink<String> get embudoString => _stringController.sink;
+
   Stream<String> get stringStream => _stringSubjet.stream;
 
-
+  /// CONSTRUCTOR DEL BLOQUE
+  /// Ponemos la "trampa" a nuestros StreamS para poder recoger lo que se vaya echando al "embudo"
   ContadoresBLoC() {
-
     _intController.stream.listen((event) {
       _enterosSubject.add(event);
     });
@@ -47,13 +65,13 @@ class ContadoresBLoC {
     });
 
     _stringController.stream.listen((event) {
-       _stringSubjet.add(event);
+      _stringSubjet.add(event);
     });
-
   }
 
+  /// ZONA DE MÉTODOS - LÓGIA DE NEGOCIO ...
 
-
+  /// DEBEMOS CERRAR LOS STREAMS PARA LIBERAR RECURSOS
   void dispose() {
     _intController.close();
     _enterosSubject.close();
@@ -66,13 +84,13 @@ class ContadoresBLoC {
     _stringSubjet.close();
   }
 
-
-
+  /// FUNCIÓN PARA AUMENTAR EN UNO EL CONTADOR DE ENTEROS
   void agregarEntero() {
     this.entero++;
     embudoEnteros.add(this.entero);
   }
 
+  /// FUNCIÓN PARA RESTAR UNO DEL CONTADOR DE ENTEROS
   void restarEntero() {
     this.entero--;
     embudoEnteros.add(this.entero);
@@ -80,36 +98,33 @@ class ContadoresBLoC {
 
 
   void agregarEnteroImpar() {
-    this.enteroImpar+=2;
+    this.enteroImpar += 2;
     embudoEnterosImpar.add(this.enteroImpar);
   }
 
   void restarEnteroImpar() {
-    this.enteroImpar-=2;
+    this.enteroImpar -= 2;
     embudoEnterosImpar.add(this.enteroImpar);
   }
 
-
   void agregarEnteroPar() {
-    this.enteroPar+=2;
+    this.enteroPar += 2;
     embudoEnterosPar.add(this.enteroPar);
   }
 
   void restarEnteroPar() {
-    this.enteroPar-=2;
+    this.enteroPar -= 2;
     embudoEnterosPar.add(this.enteroPar);
   }
 
+  /// FUNCIÓN PARA RESETAR TODOS LOS VALORES DE ATRIBUTOS O VARIALES DE LA CLASE
   void reset() {
-    this.entero=0;
-    this.enteroImpar=1;
-    this.enteroPar=2;
+    this.entero = 0;
+    this.enteroImpar = 1;
+    this.enteroPar = 2;
     embudoEnteros.add(this.entero);
     embudoEnterosImpar.add(this.enteroImpar);
     embudoEnterosPar.add(this.enteroPar);
     embudoString.add("DATA CLEARED! :)");
   }
-
-
-
 }
